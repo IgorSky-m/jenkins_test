a1750ec37305239750c6bc335f18e5fd14df41a1
Skachko Igar <scarydisco@gmail.com>
2022-12-19 14:58:56 -0600

    up

--- Jenkins.java
+++ Jenkins.java
@@ -1,9 +1,7 @@
-import java.util.Scanner;
+
 
 public class Jenkins {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
-        System.out.println(s);
+        System.out.println("Hello jenkins");
     }
 }
